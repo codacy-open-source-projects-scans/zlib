44c40b9a6668907278b763799eef766a98554df8
Maciej Harasimowicz <maciej.harasimowicz@linuxpolska.pl>
2024-12-19 01:47:30 +0000

    'Frozen autocommit 2024-12-19 01:47:30.283119'

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ This repository contains the frozen state of the repository.
 Its history is cleared. It is used only for static code
 analysis with Codacy.
 
-Commit Time: 1729835214
+Commit Time: 1734572850
--- contrib/minizip/unzip.c
+++ contrib/minizip/unzip.c
@@ -68,10 +68,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#ifndef NOUNCRYPT
-        #define NOUNCRYPT
-#endif
-
 #include "zlib.h"
 #include "unzip.h"
 
@@ -92,7 +88,7 @@
 
 
 #ifndef CASESENSITIVITYDEFAULT_NO
-#  if !defined(unix) && !defined(CASESENSITIVITYDEFAULT_YES)
+#  if (!defined(__unix__) && !defined(__unix) || defined(__CYGWIN__))  && !defined(CASESENSITIVITYDEFAULT_YES)
 #    define CASESENSITIVITYDEFAULT_NO
 #  endif
 #endif
@@ -1613,7 +1609,7 @@ extern int ZEXPORT unzReadCurrentFile(unzFile file, voidp buf, unsigned len) {
                 uInt i;
                 for(i=0;i<uReadThis;i++)
                   pfile_in_zip_read_info->read_buffer[i] =
-                      zdecode(s->keys,s->pcrc_32_tab,
+                      (char)zdecode(s->keys,s->pcrc_32_tab,
                               pfile_in_zip_read_info->read_buffer[i]);
             }
 #            endif
